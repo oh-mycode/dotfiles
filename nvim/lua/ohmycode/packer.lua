00be6864fdc4311b825583a966bc7cb208c6951c
Alexey Fedorov <ac.fedorov@gmail.com>
2022-10-16 19:25:41 +0300

    packer

--- nvim/lua/ohmycode/packer.lua
+++ nvim/lua/ohmycode/packer.lua
@@ -20,7 +20,7 @@ return require("packer").startup(function()
     use("simrat39/symbols-outline.nvim")
 
     use("TimUntersberger/neogit")
-    use({'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim'})
+    use({"sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim"})
 
     use("mbbill/undotree")
 
@@ -33,20 +33,20 @@ return require("packer").startup(function()
     use("theHamsta/nvim-dap-virtual-text")
 
     use({
-        'nvim-tree/nvim-tree.lua',
+        "nvim-tree/nvim-tree.lua",
         requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+            "nvim-tree/nvim-web-devicons", -- optional, for file icons
         },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+        tag = "nightly" -- optional, updated every week. (see issue #1193)
     })
     use({
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true }
     })
     use({
-        'akinsho/bufferline.nvim',
+        "akinsho/bufferline.nvim",
         tag = "v2.*",
-        requires = 'kyazdani42/nvim-web-devicons'
+        requires = "kyazdani42/nvim-web-devicons"
     })
     -- Colorscheme section
     use("gruvbox-community/gruvbox")
