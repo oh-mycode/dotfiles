7a46684326a31cd4d83f7855edf7e6e4da0d7fa0
Alexey Trivasser <trivasser@gmail.com>
2023-02-04 14:59:26 +0200

    one style for plugins

--- nvim/lua/ohmycode/plugins-setup.lua
+++ nvim/lua/ohmycode/plugins-setup.lua
@@ -66,8 +66,8 @@ return packer.startup(function(use)
 	use "williamboman/mason-lspconfig.nvim"
 
     -- formatting & linting
-	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	use "jose-elias-alvarez/null-ls.nvim" -- configure formatters & linters
+	use "jayp0521/mason-null-ls.nvim" -- bridges gap b/w mason & null-ls
 
     -- snippets
     use "L3MON4D3/LuaSnip"
