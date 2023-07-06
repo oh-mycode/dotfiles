944b13c802fc604af18bbcd92f2b57d8310dab8b
Alexey Trivasser <trivasser@gmail.com>
2023-07-06 11:09:11 +0300

    use lazynvim dap config

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,122 +1,159 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    keys = {
-      {
-        "<leader>cDb",
-        function()
-          require("dap").toggle_breakpoint()
-        end,
-        desc = "Toggle Breakpoint",
-      },
-      {
-        "<leader>cDr",
-        function()
-          require("dap").continue()
-        end,
-        desc = "Continue",
-      },
-      {
-        "<leader>cDo",
-        function()
-          require("dap").step_over()
-        end,
-        desc = "Step Over",
-      },
-      {
-        "<leader>cDi",
-        function()
-          require("dap").step_into()
-        end,
-        desc = "Step Into",
-      },
-      {
-        "<leader>cDu",
-        function()
-          require("dap").step_out()
-        end,
-        desc = "Step Out",
-      },
-    },
-    init = function()
-      require("dap.ext.vscode").load_launchjs(nil, { python = { "python" } })
-      vim.fn.sign_define("DapBreakpoint", { text = "🐞", texthl = "", linehl = "", numhl = "" })
-      vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      dapui.setup({})
-      local debug_win = nil
-      local debug_tab = nil
-      local debug_tabnr = nil
+  "mfussenegger/nvim-dap",
 
-      local function open_in_tab()
-        if debug_win and vim.api.nvim_win_is_valid(debug_win) then
-          vim.api.nvim_set_current_win(debug_win)
-          return
+  dependencies = {
+
+    -- fancy UI for the debugger
+    {
+      "rcarriga/nvim-dap-ui",
+      -- stylua: ignore
+      keys = {
+        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+      },
+      opts = {},
+      config = function(_, opts)
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup(opts)
+
+        local debug_win = nil
+        local debug_tab = nil
+        local debug_tabnr = nil
+
+        local function open_in_tab()
+          if debug_win and vim.api.nvim_win_is_valid(debug_win) then
+            vim.api.nvim_set_current_win(debug_win)
+            return
+          end
+
+          vim.cmd("tabedit %")
+          debug_win = vim.fn.win_getid()
+          debug_tab = vim.api.nvim_win_get_tabpage(debug_win)
+          debug_tabnr = vim.api.nvim_tabpage_get_number(debug_tab)
+
+          dapui.open({})
         end
 
-        vim.cmd("tabedit %")
-        debug_win = vim.fn.win_getid()
-        debug_tab = vim.api.nvim_win_get_tabpage(debug_win)
-        debug_tabnr = vim.api.nvim_tabpage_get_number(debug_tab)
+        local function close_tab()
+          dapui.close({})
 
-        dapui.open()
-      end
+          if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
+            vim.api.nvim_exec("tabclose " .. debug_tabnr, false)
+          end
 
-      local function close_tab()
-        dapui.close()
-
-        if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
-          vim.api.nvim_exec("tabclose " .. debug_tabnr, false)
+          debug_win = nil
+          debug_tab = nil
+          debug_tabnr = nil
         end
 
-        debug_win = nil
-        debug_tab = nil
-        debug_tabnr = nil
-      end
-
-      -- Attach DAP UI to DAP events
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        open_in_tab()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        close_tab()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        close_tab()
-      end
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = {
-      ensure_installed = { "python" },
+        -- Attach DAP UI to DAP events
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          open_in_tab()
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          close_tab()
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          close_tab()
+        end
+      end,
     },
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
+
+    -- virtual text for the debugger
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      opts = {},
+    },
+
+    -- which key integration
+    {
+      "folke/which-key.nvim",
+      optional = true,
+      opts = {
+        defaults = {
+          ["<leader>d"] = { name = "+debug" },
+          ["<leader>da"] = { name = "+adapters" },
+        },
+      },
+    },
+
+    -- mason.nvim integration
+    {
       "jay-babu/mason-nvim-dap.nvim",
+      dependencies = "mason.nvim",
+      cmd = { "DapInstall", "DapUninstall" },
+      opts = {
+        -- Makes a best effort to setup the various debuggers with
+        -- reasonable debug configurations
+        automatic_installation = true,
+
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
+
+        -- You'll need to check that you have the required things installed
+        -- online, please don't ask me how to install them :)
+        ensure_installed = {
+          "python",
+          -- Update this to ensure that you have the debuggers for the langs you want
+        },
+      },
     },
-    -- opts = {
-    --   adapter_python_path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python",
-    -- },
-    -- init = function()
-    --   require("dap-python").test_runner = "pytest"
-    -- end,
-    config = function()
-      local dap_python = require("dap-python")
-      dap_python.setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python", {})
-      dap_python.test_runner = "pytest"
-    end,
+
+    {
+      "mfussenegger/nvim-dap-python",
+      dependencies = {
+        "jay-babu/mason-nvim-dap.nvim",
+      },
+      -- opts = {
+      --   adapter_python_path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python",
+      -- },
+      -- init = function()
+      --   require("dap-python").test_runner = "pytest"
+      -- end,
+      config = function()
+        local dap_python = require("dap-python")
+        dap_python.setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python", {})
+        dap_python.test_runner = "pytest"
+      end,
+    },
+    { "nvim-telescope/telescope-dap.nvim" },
   },
-  { "nvim-telescope/telescope-dap.nvim" },
+
+  -- stylua: ignore
+  keys = {
+    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+  },
+
+  config = function()
+    local Config = require("lazyvim.config")
+    vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+    for name, sign in pairs(Config.icons.dap) do
+      sign = type(sign) == "table" and sign or { sign }
+      vim.fn.sign_define(
+        "Dap" .. name,
+        { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+      )
+    end
+  end,
+  init = function()
+    require("dap.ext.vscode").load_launchjs(nil, { python = { "python" } })
+  end,
 }
