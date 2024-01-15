0282a97ff10521fa17fe9779e0a78bde3610ed42
Alexey Trivasser <trivasser@gmail.com>
2024-01-15 21:23:41 +0200

    wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,5 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
@@ -10,15 +8,31 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = "AdventureTime"
+config.enable_tab_bar = false
+config.color_scheme = "Catppuccin Mocha"
 -- config.font = wezterm.font("Hack Nerd Font")
-config.font = wezterm.font("MesloLGS NF")
+-- config.font = wezterm.font("MesloLGS NF")
 -- config.font = wezterm.font("Mononoki Nerd Font")
-config.font_size = 14
+config.font_size = 15
 config.window_background_opacity = 1
+config.macos_window_background_blur = 30
+config.window_decorations = "RESIZE"
 
--- and finally, return the configuration to wezterm
+config.keys = {
+	{
+		key = "f",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
 return config
